94b4ef500895b777066392b13ecfbe44a1cb618b
vi000246 <vi000246@gmail.com>
2017-01-14 14:38:12 +0800

    commit

--- ThunderMoveCompleteFiles/Form1.Designer.cs
+++ ThunderMoveCompleteFiles/Form1.Designer.cs
@@ -28,12 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnBrows1 = new System.Windows.Forms.Button();
+            this.btnBrows2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numSize = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(95, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(95, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(370, 21);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 72);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "啟動";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnBrows1
+            // 
+            this.btnBrows1.Location = new System.Drawing.Point(249, 21);
+            this.btnBrows1.Name = "btnBrows1";
+            this.btnBrows1.Size = new System.Drawing.Size(75, 23);
+            this.btnBrows1.TabIndex = 1;
+            this.btnBrows1.Text = "瀏覽";
+            this.btnBrows1.UseVisualStyleBackColor = true;
+            this.btnBrows1.Click += new System.EventHandler(this.btnBrows1_Click);
+            // 
+            // btnBrows2
+            // 
+            this.btnBrows2.Location = new System.Drawing.Point(249, 70);
+            this.btnBrows2.Name = "btnBrows2";
+            this.btnBrows2.Size = new System.Drawing.Size(75, 23);
+            this.btnBrows2.TabIndex = 1;
+            this.btnBrows2.Text = "瀏覽";
+            this.btnBrows2.UseVisualStyleBackColor = true;
+            this.btnBrows2.Click += new System.EventHandler(this.btnBrows2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "迅雷下載目錄";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "已完成檔目錄";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "只搜尋大於";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(168, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "MB的檔案";
+            // 
+            // numSize
+            // 
+            this.numSize.Location = new System.Drawing.Point(95, 100);
+            this.numSize.Maximum = new decimal(new int[] {
+            1874919423,
+            2328306,
+            0,
+            0});
+            this.numSize.Name = "numSize";
+            this.numSize.Size = new System.Drawing.Size(67, 22);
+            this.numSize.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 131);
+            this.Controls.Add(this.numSize);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrows2);
+            this.Controls.Add(this.btnBrows1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnBrows1;
+        private System.Windows.Forms.Button btnBrows2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numSize;
     }
 }
 
--- ThunderMoveCompleteFiles/Form1.cs
+++ ThunderMoveCompleteFiles/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,67 @@ namespace ThunderMoveCompleteFiles
         public Form1()
         {
             InitializeComponent();
+            //取得Setting的預設值
+            textBox1.Text = (Properties.Settings.Default["DownloadPath"]??"").ToString();
+            textBox2.Text = (Properties.Settings.Default["CompletePath"]??"").ToString();
+            numSize.Value =int.Parse((Properties.Settings.Default["Size"] ?? "0").ToString());
+        }
+        //下載目錄Brows按鈕
+        private void btnBrows1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = GetBrowsePatch();
+            Properties.Settings.Default["DownloadPath"] = textBox1.Text;
+            Properties.Settings.Default.Save();
+        }
+        //已完成目錄Browse按鈕
+        private void btnBrows2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = GetBrowsePatch();
+            Properties.Settings.Default["CompletePath"] = textBox2.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        public string GetBrowsePatch() {
+            string folderPath = "";
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog1.SelectedPath;
+            }
+            return folderPath;
+        }
+
+        //按下啟動鈕
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //儲存檔案大小的值
+            Properties.Settings.Default["Size"] = numSize.Value;
+            Properties.Settings.Default.Save();
+
+
+            if (!Directory.Exists(textBox1.Text)|| !Directory.Exists(textBox2.Text))
+            {
+                MessageBox.Show("請選擇正確的下載路徑和已完成路徑!!");
+                return;
+            }
+                DirectoryInfo di = new DirectoryInfo(textBox1.Text);
+                //取得下載資料夾裡所有的資料夾
+                DirectoryInfo[] Directorys = di.GetDirectories();
+
+                //迴圈跑所有的子資料夾
+                foreach (DirectoryInfo element in Directorys)
+                {
+                    //取得子資料夾裡所有的檔
+                    var files = element.GetFiles("*", SearchOption.AllDirectories);
+                    var fileSize = numSize.Value * 1024 * 1024;//MB轉成bytes
+                    //如果資料夾裡有大於指定MB的檔 且都沒有.xltd副檔名 就搬移資料夾
+                    if (files.Where(x => x.Length > fileSize).Any(x => x.Extension != ".xltd"))
+                    {
+                        //將資料夾從原本路徑搬到已完成資料夾
+                        Directory.Move(element.FullName, textBox2.Text+"\\"+element.Name);
+                    }
+                }
+            
         }
     }
 }
